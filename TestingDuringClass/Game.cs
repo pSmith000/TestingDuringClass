11af0fa60ffc198b85fb29baa10833e72f4e668f
Peyton <peytonsemail2000@gmail.com>
2021-09-27 16:51:20 -0500

    Changed writelines to the TypeOutWords function

--- TestingDuringClass/Game.cs
+++ TestingDuringClass/Game.cs
@@ -367,25 +367,25 @@ namespace HelloDungeonExpanded
                 //this could either be good or bad for the player
                 //this is the first room
                 
-                Console.WriteLine("You walk in to a dimly lit room. A man stands before you bloodied and bruised." +
+                TypeOutWords("You walk in to a dimly lit room. A man stands before you bloodied and bruised." +
                     "\n He holds a sword in his hand. He slowly face you and says 'r-r-re b-boot?' The man then" +
-                    "\n lunges at you.\n");
+                    "\n lunges at you.\n", 15);
                 if (_player.CurrentItem.Type == ItemType.ATTACK)
                 {
-                    Console.WriteLine("You pull out your handgun and shoot the man in the chest. He pulls back and tilts his head at you." +
-                        "\n 're...boot...' He turns away and as he does his body siezes and falls to the ground. He is dead.\n");
-                    Console.WriteLine("SUSTAIN 5 SANITY LOSS");
+                    TypeOutWords("You pull out your handgun and shoot the man in the chest. He pulls back and tilts his head at you." +
+                        "\n 're...boot...' He turns away and as he does his body siezes and falls to the ground. He is dead.\n", 20);
+                    TypeOutWords("SUSTAIN 5 SANITY LOSS", 50);
                     _player.GainInfection(5);
                     _player.LoseSanity(5);
                 }
                 else
                 {
-                    Console.WriteLine("The man swings his sword widly at you. With no weapon yourself you are defenseless." +
+                    TypeOutWords("The man swings his sword widly at you. With no weapon yourself you are defenseless." +
                         "\n He madly slashes your chest and arms screaming the same word over and over again." +
-                        "\n re..boot   rebOOT   REBOOT   ??REE??BOOTT?????REEEB?OO?TTT\n He collapses to the floor in the middle of one of his swings at you.\n");
-                    Console.WriteLine("You take severe injuries");
-                    Console.WriteLine("SUSTAIN 35 DAMAGE");
-                    Console.WriteLine("SUSTAIN 15 SANITY LOSS");
+                        "\n re..boot   rebOOT   REBOOT   ??REE??BOOTT?????REEEB?OO?TTT\n He collapses to the floor in the middle of one of his swings at you.\n", 25);
+                    TypeOutWords("You take severe injuries", 50);
+                    TypeOutWords("SUSTAIN 35 DAMAGE", 50);
+                    TypeOutWords("SUSTAIN 15 SANITY LOSS", 50);
                     _player.GainInfection(5);
                     _player.TakeDamage(35);
                     _player.LoseSanity(15);
@@ -394,54 +394,54 @@ namespace HelloDungeonExpanded
             //The second room
             else if (room1 == 2)
             {
-                Console.WriteLine("You walk in to a room with a roaring fireplace and lush carpets. On the table you find a medkit and heal 20 health!");
+                TypeOutWords("You walk in to a room with a roaring fireplace and lush carpets. On the table you find a medkit and heal 20 health!", 25);
                 _player.TakeDamage(-20);
             }
             //the third room
             else if (room1 == 3)
             {
-                Console.WriteLine("You enter in a sanctuary of sorts. There are pews, symbolic pieces, and scripture on the walls. It feels safe. You regain 20 sanity.");
+                TypeOutWords("You enter in a sanctuary of sorts. There are pews, symbolic pieces, and scripture on the walls. It feels safe. You regain 20 sanity.", 25);
                 _player.LoseSanity(-20);
                 
             }
             //the fourth room
             else if (room1 == 4)
             {
-                Console.WriteLine("You walk in to a room that smells strongly of iron. It is too dark to see but the walls are covered in a warm liquid. \n" +
-                    "While it does make you nauseous, you are safe.\n");
-                Console.WriteLine("SUSTAIN 30 SANITY LOSS");
+                TypeOutWords("You walk in to a room that smells strongly of iron. It is too dark to see but the walls are covered in a warm liquid. \n" +
+                    "While it does make you nauseous, you are safe.\n", 25);
+                TypeOutWords("SUSTAIN 30 SANITY LOSS", 50);
                 _player.LoseSanity(30);
             }
             //the fifth room
             else if (room1 == 5)
             {
-                Console.WriteLine("You stumble into a long hallway. As you walk down this dim lit path you find a sword lying on the ground.\n" +
-                    "You may take it if you do not have a weapon already.");
+                TypeOutWords("You stumble into a long hallway. As you walk down this dim lit path you find a sword lying on the ground.\n" +
+                    "You may take it if you do not have a weapon already.", 25);
             }
             //the sixth room
             else if (room1 == 6)
             {
-                Console.WriteLine("Walking into this room you smell tar and asphalt. The air is humid and thick. Breathing is an almost impossible task. " +
-                    "\nYou see a vial of purple sludge on the ground half spilled over. You inspect it. \n");
+                TypeOutWords("Walking into this room you smell tar and asphalt. The air is humid and thick. Breathing is an almost impossible task. " +
+                    "\nYou see a vial of purple sludge on the ground half spilled over. You inspect it. \n", 25);
 
-                int input = GetInput("Would you like to drink?", "Yes", "No");
+                int input = GetInput("Would you like to drink?\n", "Yes", "No");
 
-                if (input == 1)
+                if (input == 0)
                 {
-                    Console.WriteLine("\nYou decide to drink this putrid liquid. As it goes down your esophagus it has the taste of rotting flesh and the consistancy of spoiled milk." +
-                        "\nImmediately you vomit. What comes out is a viscous black liquid. As this matter leaves your body, your mind feels less cluttered.");
+                    TypeOutWords("\nYou decide to drink this putrid liquid. As it goes down your esophagus it has the taste of rotting flesh and the consistancy of spoiled milk." +
+                        "\nImmediately you vomit. What comes out is a viscous black liquid. As this matter leaves your body, your mind feels less cluttered.", 25);
                     _player.GainInfection(-15);
                 }
 
-                else if (input == 2)
+                else if (input == 1)
                 {
-                    Console.WriteLine("\nYou use your better judgement and decide not to drink the vial of mysterious liquid." +
-                        "\nYou move forward.");
+                    TypeOutWords("\nYou use your better judgement and decide not to drink the vial of mysterious liquid." +
+                        "\nYou move forward.", 25);
                 }
             }
             Console.WriteLine("");
 
-            Console.WriteLine("Press ENTER to continue");
+            TypeOutWords("Press ENTER to continue", 50);
             Console.ReadKey();
             Console.Clear();
         }
